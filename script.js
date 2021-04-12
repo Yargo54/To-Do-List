71b57ec0abc48d6f9a878607e173bce29aaa013c
yudin yaroslav <yarikyudin@mail.ru>
2021-04-12 15:47:47 +0300

    workaddbutton

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let form = document.querySelector('.toDoListForm');
 let icon = document.querySelector('.sortImage');
-let newItem = 
+let newItem = document.querySelector('.buttonAdd');
+let inputVal = document.querySelector('.inputToDo');
+let del = document.querySelector('.delete'); 
+let add = document.querySelector('.add')
+let divInput = document.querySelector('.divInput')
+let maintodo = document.querySelector('.mainToDo')
+
+function Add(){
+    let addElem = divInput.cloneNode(true);
+    add.appendChild(addElem);
+    inputVal.value = '';
+    maintodo.style.height = '1%';
+}
+
+newItem.addEventListener('click', () =>{
+    Add();
+})
 
 icon.addEventListener('click', () =>{
     if (icon.value === '1'){
@@ -12,14 +28,14 @@ icon.addEventListener('click', () =>{
     }
 })
 
+del.addEventListener('mousedown', ()=>{
+    if(inputVal.id === "0"){
+        del.style.backgroundColor = "indigo"
+        inputVal.value = '';  
+        console.log('del')
+    }
+})
 
-
-// form.addEventListener('click', () => {
-//     if (icon.value === '1'){
-//         icon.style.background = "url('" +"./images/NotFilledSortUp.png" + "')";
-//         icon.value = '2';
-//     } else if(icon.value === '2') {
-//         icon.style.background = "url('" +"./images/NotFilledSortDown.png" + "')";
-//         icon.value = '1';
-//     }
-// })
+del.addEventListener('mouseup', ()=>{
+    del.style.backgroundColor = "white"
+})
